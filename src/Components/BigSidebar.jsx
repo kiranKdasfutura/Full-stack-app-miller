4563978f8143de01fbad98b013b599e9993bf095
kirandream7777 <kirandream7777@gamil.com>
2023-10-24 14:49:37 +0530

    check in bigsidebar

--- src/Components/BigSidebar.jsx
+++ src/Components/BigSidebar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
-
+//check
 const BigSidebar = () => {
   return (
     <Wrapper>
       <div>BigSidebar</div>
+      
     </Wrapper>
   );
 };
